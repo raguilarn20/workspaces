d60c997d44f29817904e2ae5b4eb0130f84e0436
Ricard Aguilar Navarro <raguilarn20@ilg.cat>
2021-11-15 19:13:41 +0000

    Ricard Aguilar: vscode nivell1

---
+++ java/Wrapper/src/paqueteintegers/App.java
@@ -0,0 +1,7 @@
+package paqueteintegers;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Wrapper intWrapper = new Wrapper(5);
+
+    }
+}
---
+++ java/Wrapper/src/paqueteintegers/Wrapper.java
@@ -0,0 +1,14 @@
+package paqueteintegers;
+
+
+public class Wrapper {
+    private final int[] arrayInteger;
+
+    public Wrapper(int arraySize) {
+        this.arrayInteger= new int[arraySize];
+    }
+    
+
+
+    
+}
